d3c05675f78ad0c4aa3fb473b6e3370e9a833c24
Omkar Panchal <86731206+Omkar078@users.noreply.github.com>
2023-03-02 02:13:56 +0530

    Add files via upload

---
+++ Bike_sales_dataset_custom.sql
@@ -0,0 +1,36 @@
+
+SELECT
+    o.order_id,
+	CONCAT(c.first_name,' ', c.last_name) AS 'customer_name',
+	c.city,
+	c.state,
+	o.order_date,
+	SUM(oi.quantity) AS 'total_price',
+	SUM(oi.quantity * oi.list_price) AS 'revenue',
+	p.product_name,
+	pc.category_name,
+	s.store_name,
+	CONCAT(ss.first_name, ' ', ss.last_name) AS 'sales_representative'
+FROM sales.orders o
+INNER JOIN sales.customers c
+ON o.customer_id = c.customer_id
+INNER JOIN sales.order_items oi
+ON o.order_id = oi.order_id
+INNER JOIN production.products p
+ON oi.product_id = p.product_id
+INNER JOIN production.categories pc
+ON p.category_id = pc.category_id
+INNER JOIN sales.stores s
+ON o.store_id = s.store_id
+INNER JOIN sales.staffs ss
+ON o.staff_id = ss.staff_id
+GROUP BY
+	o.order_id,
+	CONCAT(c.first_name,' ', c.last_name),
+	c.city,
+	c.state,
+	o.order_date,
+	p.product_name,
+	pc.category_name,
+	s.store_name,
+	CONCAT(ss.first_name, ' ', ss.last_name)
